2f2807ff08c5a87ce2778bd4bfa86163a26202bb
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-07-02 10:58:04 -0400

    Implemented Next-auth functionality

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,12 @@
+'use client';
+
 import { TbPrompt } from "react-icons/tb";
 import { HiHome } from "react-icons/hi";
 import Link from 'next/link';
+import {useSession,signOut,signIn} from 'next-auth/react';
 
 export const Sidebar = () => {
+  const {data:session}=useSession();
   return (
     <div className="flex flex-col items-center gap-4 pr-3">
       <Link href='/'>
@@ -14,9 +18,17 @@ export const Sidebar = () => {
         <HiHome className="w-7 h-7"/>
         <span className="font-bold hidden xl:inline">Home</span>
       </Link>
-      <button className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hiden xl:inline">
+      {session?(
+        <button className="bg-red-600 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hiden xl:inline font-semibold"
+        onClick={()=>signOut()}>
+        Sign Out
+      </button>
+      ):(
+        <button className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hiden xl:inline font-semibold"
+        onClick={()=>signIn()}>
         Sign In
       </button>
+      )}
     </div>
   );
 }
