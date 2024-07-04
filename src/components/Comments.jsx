3c97cd586ce5d523529fe15bc3b3444574768f0e
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-07-04 08:49:40 -0400

    completed the comment likes of the post

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,36 +1,42 @@
 'use client';
 
 import {
-    collection,
-    getFirestore,
-    onSnapshot,
-    orderBy,
-    query,
+  collection,
+  getFirestore,
+  onSnapshot,
+  orderBy,
+  query,
 } from 'firebase/firestore';
-import {app} from '../firebase';
+import { app } from '../firebase';
 import { useEffect, useState } from 'react';
 import Comment from './Comment';
 
-export default function Comments({id}) {
-    const db=getFirestore(app);
-    const[comments, setComments]= useState([]);
-    useEffect(()=>{
-        onSnapshot(
-            query(
-                collection(db,'posts',id,'comments'),
-                orderBy('timestamp',"desc")
-            ),
-            (snapshot)=>{
-                setComments(snapshot.docs);
-            }
-        );
-    },[db,id]);
+export default function Comments({ id }) {
+  const db = getFirestore(app);
+  const [comments, setComments] = useState([]);
 
+  useEffect(() => {
+    onSnapshot(
+      query(
+        collection(db, 'posts', id, 'comments'),
+        orderBy('timestamp', 'desc')
+      ),
+      (snapshot) => {
+        setComments(snapshot.docs);
+      }
+    );
+  }, [db, id]);
+  
   return (
     <div>
-        {comments.map((comment)=>(
-            <Comment key={comment.id} comment={comment.data()} id={comments.id}/>
-        ))}
+      {comments.map((comment) => (
+        <Comment
+          key={comment.id}
+          comment={comment.data()}
+          commentId={comment.id}
+          originalPostId={id}
+        />
+      ))}
     </div>
   );
 }
