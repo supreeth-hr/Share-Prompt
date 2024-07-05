a5aabb2c05d267e4c2cc29f7004374f7919dbeab
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-07-05 11:56:38 -0400

    CSS changes to layouts

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Feed from '@/components/Feed';
 
 export default function page() {
   return (
-    <div className='max-w-xl mx-auto border-r border-l min-h-screen'>
+    <div className='mx-auto border-r border-l min-h-screen'>
       <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
         <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
       </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,35 +1,42 @@
 'use client';
-
-import { TbPrompt } from "react-icons/tb";
 import { HiHome, HiDotsHorizontal } from "react-icons/hi";
 import Link from 'next/link';
+import Image from "next/image";
 import { useSession, signOut, signIn } from 'next-auth/react';
 
 export default function Sidebar() {
   const { data: session } = useSession();
   return (
-    <div className='flex flex-col p-3 justify-between h-screen'>
-      <div className='flex flex-col gap-4 '>
+    <div className='flex flex-col p-3 justify-between items-end h-screen'>
+      <div className=' ml-10 flex flex-col gap-4'>
         <Link href='/'>
-          <TbPrompt className='w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200 ' />
+        <div className="pr-8 pt-8">
+          <Image
+            src='/images/logo-text.svg'
+            alt='logo'
+            width={230}
+            height={150}
+            className='object-contain'
+          />
+        </div>
         </Link>
-        <Link
-          href='/'
-          className='flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit'
-        >
-          <HiHome className='w-7 h-7' />
-          <span className='font-bold hidden xl:inline'>Home</span>
-        </Link>
+          <Link
+            href='/'
+            className='flex pt-6 pl-12 items-end transition-all duration-200 gap-1 w-fit'
+          >
+            <HiHome className='w-7 h-7' />
+            <span className='font-bold hidden xl:inline'>Home</span>
+          </Link>
         {session ? (
           <button
-            className='bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'
+            className='bg-red-600 text-white rounded-full mt-4  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'
             onClick={() => signOut()}
           >
             Sign Out
           </button>
         ) : (
           <button
-            className='bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'
+            className='bg-blue-400 text-white rounded-full mt-4 hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'
             onClick={() => signIn()}
           >
             Sign In
@@ -37,7 +44,7 @@ export default function Sidebar() {
         )}
       </div>
       {session && (
-        <div className='text-gray-700 text-sm flex items-center cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200'>
+        <div className='text-gray-700 text-sm flex mr-9 items-center cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200'>
           <img
             src={session.user.image}
             alt='user-img'
@@ -47,7 +54,6 @@ export default function Sidebar() {
             <h4 className='font-bold'>{session.user.name}</h4>
             <p className='text-gray-500'>@{session.user.username}</p>
           </div>
-          <HiDotsHorizontal className='h-5 xl:ml-8 hidden xl:inline' />
         </div>
       )}
     </div>
