ec1a9b2950253a3ce534f610d51b04413726a19f
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-07-05 16:47:37 -0400

    Name change

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ import CommentModal from "@/components/CommentModal";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Prompt Sphere",
+  title: "Share Prompt",
   description: "Developed by Supreeth",
 };
 
