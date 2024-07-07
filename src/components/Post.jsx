622d4082009accb0a56bdeb6c40772027e755e3d
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-07-06 22:18:52 -0400

    Implemented image zoom feature of the post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,16 @@
+'use client'
+
 import Link from 'next/link';
 import Icons from './Icons';
+import { useState } from 'react';
 
 export default function Post({post, id}) {
+    const [showModal, setShowModal] = useState(false);
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
+      };
+
   return (
     <div className='flex p-3 border-b border-gray-200 hover:bg-gray-50'>
         <img
@@ -33,12 +42,35 @@ export default function Post({post, id}) {
                 </p>
             </Link>
 
-            <Link href={`/posts/${id}`}>
-                <img src={post?.image}
-                className='rounded-2xl mr-2'/>
-            </Link>
+          <div className='cursor-pointer'>
+            <img
+                src={post?.image}
+                className='rounded-2xl mr-2 cursor-pointer'
+                onClick={toggleModal}
+            />
+          </div>
+
             <Icons id={id} uid={post.uid}/>
         </div>
+
+        {showModal && (
+        <div className='fixed inset-0 z-50 flex items-center justify-center overflow-auto bg-black bg-opacity-50'>
+          <div className='max-w-full p-4 bg-white rounded-lg shadow-lg'>
+            <img
+              src={post?.image}
+              alt='modal-image'
+              className='max-w-full max-h-screen rounded-lg'
+            />
+            <button
+              className='absolute top-4 right-4 text-white hover:text-gray-400'
+              onClick={toggleModal}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+
     </div> 
   );
 }
